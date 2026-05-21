785160cd43632474d2f16dff653cbebfbfbe8cc1
FOA-developer <flourishagbanoma@gmail.com>
2026-05-21 11:05:05 +0100

    feat: added Nestly logo svg component

---
+++ frontend/src/components/NestlyLogo.jsx
@@ -0,0 +1,15 @@
+const NestlyLogo = () => {
+  return ( 
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 60" fill="none">
+      <path
+        d="M50 30 C50 30 38 10 20 10 C8 10 0 18 0 30 C0 42 8 50 20 50 C38 50 50 30 50 30 C50 30 62 10 80 10 C92 10 100 18 100 30 C100 42 92 50 80 50 C62 50 50 30 50 30 Z"
+        stroke="#2C2523"
+        stroke-width="3"
+        fill="transparent"
+      />
+    </svg>
+   );
+}
+ 
+export default NestlyLogo;
+
