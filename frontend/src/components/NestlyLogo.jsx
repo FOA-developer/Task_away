5b2ea9434f9cd88fc39c2b7ef78cfaaaf1fec494
FOA-developer <flourishagbanoma@gmail.com>
2026-05-21 11:32:20 +0100

    feat: updated svg design by reducing size

--- frontend/src/components/NestlyLogo.jsx
+++ frontend/src/components/NestlyLogo.jsx
@@ -1,10 +1,10 @@
 const NestlyLogo = () => {
   return ( 
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 60" fill="none">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 100" width="30" height="20" fill="none">
       <path
-        d="M50 30 C50 30 38 10 20 10 C8 10 0 18 0 30 C0 42 8 50 20 50 C38 50 50 30 50 30 C50 30 62 10 80 10 C92 10 100 18 100 30 C100 42 92 50 80 50 C62 50 50 30 50 30 Z"
+        d="M30 50 C30 50 10 38 10 20 C10 8 18 0 30 0 C42 0 50 8 50 20 C50 38 30 50 30 50 C30 50 10 62 10 80 C10 92 18 100 30 100 C42 100 50 92 50 80 C50 62 30 50 30 50 Z"
         stroke="#2C2523"
-        stroke-width="3"
+        strokeWidth="3"
         fill="transparent"
       />
     </svg>
