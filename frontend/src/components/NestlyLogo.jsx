d8d7cebff3542c05b800e09213d9ede6337794c0
FOA-developer <flourishagbanoma@gmail.com>
2026-05-22 22:01:43 +0100

    feat: updated logo design

--- frontend/src/components/NestlyLogo.jsx
+++ frontend/src/components/NestlyLogo.jsx
@@ -1,14 +1,31 @@
 const NestlyLogo = () => {
   return ( 
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 100" width="30" height="20" fill="none">
+    <svg width="30" height="30" viewBox="0 0 64 64" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
-        d="M30 50 C30 50 10 38 10 20 C10 8 18 0 30 0 C42 0 50 8 50 20 C50 38 30 50 30 50 C30 50 10 62 10 80 C10 92 18 100 30 100 C42 100 50 92 50 80 C50 62 30 50 30 50 Z"
-        stroke="#2C2523"
-        strokeWidth="3"
-        fill="transparent"
+        d="M32 10C28 18 25 24 25 31C25 38 28 43 32 46C36 43 39 38 39 31C39 24 36 18 32 10Z"
+        stroke="#1A1414"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      
+      <path
+        d="M25 31C18 28 13 26 8 26C8 38 15 48 26 54"
+        stroke="#1A1414"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+
+      <path
+        d="M39 31C46 28 51 26 56 26C56 38 49 48 38 54"
+        stroke="#1A1414"
+        stroke-width="3"
+        stroke-linecap="round"
+        stroke-linejoin="round"
       />
     </svg>
-   );
+   )
 }
  
 export default NestlyLogo;
