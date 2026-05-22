e2b95d0fa65072bf2116d49e77100d7ef67de0cd
FOA-developer <flourishagbanoma@gmail.com>
2026-05-22 23:45:13 +0100

    feat: added new routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import LoginPage from "./pages/public/LoginPage"
+import SignUpPage from "./pages/public/SignUpPage"
 
 
 function App() {
 
   return (
     <>
-      
+      <Routes>
+        <Route path="/login" element={<LoginPage/>} /> 
+        <Route path="/signup" element={<SignUpPage/>} /> 
+        <Route path="*" element={<h1>404 - Not Found</h1>} />
+      </Routes>
     </>
   )
 }
