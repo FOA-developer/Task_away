c04b82bd3a3131104b958bba721643673656a3fc
FOA-developer <flourishagbanoma@gmail.com>
2026-05-22 22:03:30 +0100

    feat: added new styles to button component

--- frontend/src/components/shared/Button.jsx
+++ frontend/src/components/shared/Button.jsx
@@ -1,10 +1,10 @@
 const Button = ({ size = "small", children}) => {
 
-  const baseStyle =  "rounded-full text-white bg-primary";
+  const baseStyle =  "rounded-full text-white bg-primary m-auto";
   
   const sizes ={
     small: "text-sm",
-    large: "text-base"
+    large: "text-base font-semibold tracking-wide py-4 w-[450px]"
   }
 
   return ( 
