100c37b7501ea17e0cc85629eecd27c6ffbc2c4a
FOA-developer <flourishagbanoma@gmail.com>
2026-05-23 23:37:43 +0100

    feat: added the route for the landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import LoginPage from "./pages/public/LoginPage"
 import SignUpPage from "./pages/public/SignUpPage"
+import LandingPage from './pages/public/LandingPage'
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Routes>
+        <Route path="/welcome" element={<LandingPage/>} />
         <Route path="/login" element={<LoginPage/>} /> 
         <Route path="/signup" element={<SignUpPage/>} /> 
         <Route path="*" element={<h1>404 - Not Found</h1>} />
