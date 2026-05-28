ab5954eece69ed2fc7699eda2a320cb6d277f820
FOA-developer <flourishagbanoma@gmail.com>
2026-05-28 15:40:57 +0100

    feat: update Hero component for improved structure and add HowItWorks to LandingPage

--- frontend/src/components/landing/Hero.jsx
+++ frontend/src/components/landing/Hero.jsx
@@ -1,15 +1,16 @@
-import Navbar from "./Navbar.jsx"
-import Button from "../shared/Button.jsx"
+import Navbar from "./Navbar.jsx";
+import Button from "../shared/Button.jsx";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
   return ( 
     <div className="grid-background min-h-screen text-primary">
       <section className="relative z-10 flex flex-col" id="Home">
         <Navbar/>
-        <div className="flex flex-col justify-center items-center pt-20">
-          <h1 className="text-7xl text-center font-playfair loading-loose">Your tasks, your rhythm.<br></br>Stay in flow.</h1>
-          <p className="m-auto w-[50%] text-center pt-7 text-grey">Organize your life beautifully, collaborate effortlessly, and never lose track of what matters. A mindful approach to productivity that respects your time and energy.</p>
-          <Button className="mt-6  transition duration-" size="medium">Get Started</Button>
+        <div className="flex flex-col justify-center items-center pt-20 text-center">
+          <h1 className="text-7xl font-playfair loading-loose">Your tasks, your rhythm.<br></br>Stay in flow.</h1>
+          <p className="m-auto w-[50%] pt-7 text-grey">Organize your life beautifully, collaborate effortlessly, and never lose track of what matters. A mindful approach to productivity that respects your time and energy.</p>
+          <Button className="mt-6  transition duration-" size="medium"><Link to="/login">Get Started</Link></Button>
         </div>
       </section>
     </div>
--- frontend/src/pages/public/LandingPage.jsx
+++ frontend/src/pages/public/LandingPage.jsx
@@ -1,6 +1,11 @@
+import Hero from "../../components/landing/Hero";
+import HowItWorks from "../../components/landing/HowItWorks";
+
 const LandingPage = ( ) => {
   return(
     <>
+    <Hero/>
+    <HowItWorks/>
     </>
   )
 }
