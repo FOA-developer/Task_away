e3126e9b87ffd3195b9c790e64396de65d5d2a3e
FOA-developer <flourishagbanoma@gmail.com>
2026-05-28 13:48:53 +0100

    feat: enhance Hero and Navbar components with responsive design and additional features

--- frontend/src/components/landing/Hero.jsx
+++ frontend/src/components/landing/Hero.jsx
@@ -1,10 +1,16 @@
 import Navbar from "./Navbar.jsx"
+import Button from "../shared/Button.jsx"
 
 const Hero = () => {
   return ( 
-    <div className="grid-background min-h-screen">
-      <section className="relative z-10">
+    <div className="grid-background min-h-screen text-primary">
+      <section className="relative z-10 flex flex-col" id="Home">
         <Navbar/>
+        <div className="flex flex-col justify-center items-center pt-20">
+          <h1 className="text-7xl text-center font-playfair loading-loose">Your tasks, your rhythm.<br></br>Stay in flow.</h1>
+          <p className="m-auto w-[50%] text-center pt-7 text-grey">Organize your life beautifully, collaborate effortlessly, and never lose track of what matters. A mindful approach to productivity that respects your time and energy.</p>
+          <Button className="mt-6  transition duration-" size="medium">Get Started</Button>
+        </div>
       </section>
     </div>
    );
--- frontend/src/components/landing/Navbar.jsx
+++ frontend/src/components/landing/Navbar.jsx
@@ -1,17 +1,52 @@
-import NestlyLogo from "../NestlyLogo";
-const links = ["About", "How it Works", "Features", "Login"]
+import NestlyLogo from "../NestlyLogo.jsx";
+import Button from "../shared/Button.jsx";
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { MdClose } from 'react-icons/md';
+import {useState} from "react"
+ const links = [
+        { name:"Home",
+          link: "#Home",
+        }, 
+        {name:"How it Works",
+          link: "#howitworks",
+        },
+        {name: "Features",
+          link: "",
+        }, 
+        { name: "Login",
+          link: "",
+        }
+      ]
+
 
 const Navbar = () => {
 
+  const[isOpen, setIsOpen] = useState(false)
+
   return (
-    <navbar className="flex flex-row">
-      <div>
-        <NestlyLogo/>
-        <h3 className="">Nestly</h3>
+    <navbar className="flex flex-row p-5 justify-between">
+      <div className="flex flex-row gap-1 pl-2 pt-1">
+        <NestlyLogo />
+        <h3 className="font-semibold text-lg">Nestly</h3>
       </div>
-      <div>
-        
+      <div className="flex flex-row gap-10 justify-between pr-6">
+        <ul className="hidden md:flex flex-row gap-5 align-middle pt-2">
+          {links.map((link) => {
+            return <li className="font-semibold " key={link.name}><a href={link.link}>{link.name}</a></li>
+          })}
+        </ul>
+        <Button className="align-top" size="small">Get Started</Button>
       </div>
+      <button className="flex md:hidden" onClick={() => setIsOpen(!isOpen)}>
+        {isOpen ? <GiHamburgerMenu/> : <MdClose/>}
+      </button>
+      {isOpen && (
+          <div className="absolute top-14 right-0 w-full rounded-lg flex flex-col items-center gap-6 py-8 z-50">
+            {links.map((link) => {
+              return <a href={link.link} key={link.name} onClick={() => setIsOpen(false)} className="text-white hover:text-[#FE4E02] transition duration-300">{link.name}</a>
+            })}
+          </div>
+        )}
     </navbar>
    );
 }
