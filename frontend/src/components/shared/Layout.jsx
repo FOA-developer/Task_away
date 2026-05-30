911a2e73c96c8d05c541955b1f47caba19234cb7
FOA-developer <flourishagbanoma@gmail.com>
2026-05-30 23:48:38 +0100

    feat: created the layput fot the navbar for the mobile and desktop

---
+++ frontend/src/components/shared/Layout.jsx
@@ -0,0 +1,13 @@
+import Sidebar from "./Sidebar";
+import { LayoutDashboardIcon, Calendar1Icon, ClipboardCheckIcon, Users2Icon,  SettingsIcon } from "lucide-react";
+import NestlyLogo from "../NestlyLogo.jsx";
+
+const Layout = ({children}) => {
+  return ( 
+    <div className="flex flex-col h-screen">
+      <header className="flex md:hidden"></header>
+    </div>
+   );
+}
+ 
+export default Layout;
