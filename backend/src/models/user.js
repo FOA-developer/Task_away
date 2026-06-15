1ef90aa64763da53a14c09285aab039bade1bbae
FOA-developer <flourishagbanoma@gmail.com>
2026-06-15 17:47:35 +0100

    fix: correct typo in comparePasswords method parameter name

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -40,7 +40,7 @@ userSchema.pre("save", async function(next){
   next()
 })
 
-userSchema.methods.comparePasswords = async function (passowrd) {
+userSchema.methods.comparePasswords = async function (password) {
   return await bcrypt.compare(password, this.password)
 }
 
