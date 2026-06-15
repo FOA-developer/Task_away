4d31e8d1246bea54cdae6c8641866a48b97491f7
FOA-developer <flourishagbanoma@gmail.com>
2026-06-15 17:46:38 +0100

    feat: set up express server with JSON parsing and authentication routes

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
 import express from "express";
+import authRouter from "./src/routes/user.js"
 
 const app = express();
+
+app.use(express.json())
+app.use("/api/auth", authRouter )
+
 
 
 export default app;
