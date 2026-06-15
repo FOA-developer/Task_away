d4c9d88686ac6e865e6a902a1d678165bfe4e196
FOA-developer <flourishagbanoma@gmail.com>
2026-06-15 16:27:30 +0100

    fix: correct password handling and improve user registration logic

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -2,8 +2,69 @@ import { User } from "../models/user.js";
 
 const registerUser = async (req , res) => {
   try{
-    const{name, email, passwword} = req.body;
+    const{name, email, password, confirmPassword} = req.body;
+    if(!name || !email || !password || !confirmPassword){
+      return res.status(400).json({
+        message: "All fields must be filled"
+      })
+    }
 
-    
+    if(password !== confirmPassword){
+      return res.status(400).json({
+        message: "Passwords do not match"
+      })
+    }
+
+    const existing = await User.findOne({email: email.toLowerCase()});
+    if(existing){
+      return res.status(403).json({
+        message: "user already exists"
+      })
+    }
+
+    const user = await User.create({
+      name,
+      email: email.toLowerCase(),
+      password,
+    })
+    return res.status(201).json({
+      message: "User created successfully",
+      id: user._id,
+      name : user.name,
+      email: user.email
+    })
   }
+  catch(err){
+    return res.status(500).json({
+      message: "Error creating user",
+      error: err.message
+    })
+  }
+}
+
+const loginUser = async (req, res) => {
+  try{
+    const {email, password} = req.body;
+     if(!password || !email){
+      return res.status(400).json({
+        message: "All fields must be filled"
+      })
+     }
+
+     const existing = await User.findOne({email: email.toLowerCase()});
+     if(!existing){
+      return res.status(403).json({
+        messsage: "User dosen't exist...sign up"
+      })
+     }
+
+     const isMatch = await existing.comparePassword(password);
+  }catch{
+
+  }
+}
+
+export default {
+  registerUser,
+  loginUser
 }
