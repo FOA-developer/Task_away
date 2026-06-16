5a2835f50c4bc8c8de1f0a122401ec69ef03555e
FOA-developer <flourishagbanoma@gmail.com>
2026-06-16 16:34:32 +0100

    feat: implement Tasks page layout with header

--- frontend/src/pages/private/Tasks.jsx
+++ frontend/src/pages/private/Tasks.jsx
@@ -2,7 +2,9 @@ import Layout from "../../components/shared/Layout.jsx";
 
 const Tasks = () => {
   return (  
-    <></>
+    <Layout>
+      <h1 className="text-2xl font-bold text-primary">Tasks Page</h1>
+    </Layout>
   );
 }
  
