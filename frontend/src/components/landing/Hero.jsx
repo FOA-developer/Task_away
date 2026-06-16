cf7d6213490b1af992ecc421ef7a651530e853f6
FOA-developer <flourishagbanoma@gmail.com>
2026-06-16 17:01:36 +0100

    fix: update text color in Hero component description

--- frontend/src/components/landing/Hero.jsx
+++ frontend/src/components/landing/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <Navbar/>
         <div className="flex flex-col justify-center items-center pt-20 text-center">
           <h1 className="text-7xl font-playfair loading-loose">Your tasks, your rhythm.<br></br>Stay in flow.</h1>
-          <p className="m-auto w-[50%] pt-7 text-grey">Organize your life beautifully, collaborate effortlessly, and never lose track of what matters. A mindful approach to productivity that respects your time and energy.</p>
+          <p className="m-auto w-[50%] pt-7 text-primary">Organize your life beautifully, collaborate effortlessly, and never lose track of what matters. A mindful approach to productivity that respects your time and energy.</p>
           <Button className="mt-6  transition duration-" size="medium"><Link to="/login">Get Started</Link></Button>
         </div>
       </section>
