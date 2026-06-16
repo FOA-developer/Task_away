46e30770913ece0309977b0cb757c0803aaa9db6
FOA-developer <flourishagbanoma@gmail.com>
2026-06-16 13:14:19 +0100

    feat: enable CORS support for API requests

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors"
 import authRouter from "./src/routes/user.js"
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use("/api/auth", authRouter )
 
