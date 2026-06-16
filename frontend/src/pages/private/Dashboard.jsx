b1210accc06c9086213d84e5b01a232f39ada19a
FOA-developer <flourishagbanoma@gmail.com>
2026-06-16 17:00:35 +0100

    feat: implement Dashboard page layout with header

--- frontend/src/pages/private/Dashboard.jsx
+++ frontend/src/pages/private/Dashboard.jsx
@@ -2,7 +2,9 @@ import Layout from "../../components/shared/Layout.jsx";
 
 const Dashoard = () => {
   return ( 
-    <></>
+    <Layout>'
+      <h1 className="text-2xl font-bold text-primary">Dashboard Page</h1>
+    </Layout>
    );
 }
  
