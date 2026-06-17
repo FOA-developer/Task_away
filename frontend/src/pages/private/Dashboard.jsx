6223a1cbe6b2d59b2fea75e907d8f2be81934a8d
FOA-developer <flourishagbanoma@gmail.com>
2026-06-17 23:38:06 +0100

    feat: update Dashboard page title and structure for personal workspace

--- frontend/src/pages/private/Dashboard.jsx
+++ frontend/src/pages/private/Dashboard.jsx
@@ -3,7 +3,10 @@ import Layout from "../../components/shared/Layout.jsx";
 const Dashoard = () => {
   return ( 
     <Layout>'
-      <h1 className="text-2xl font-bold text-primary">Dashboard Page</h1>
+      <div>
+      <h1 className="text-2xl font-bold text-primary">Personal Workspace</h1>
+      <p></p>
+      </div>
     </Layout>
    );
 }
