58976754eadeeac90de24c8f9b7619c13456243c
FOA-developer <flourishagbanoma@gmail.com>
2026-06-17 16:29:52 +0100

    feat: add workspace model with schema for workspace management

---
+++ backend/src/models/workspace.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const workspaceSchema = new Schema({
+  name:{
+    type: String,
+    required: true,
+    maxLength: 300,
+    minLength: 3,
+  },
+  owner:{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  members: [{
+    type: [mongoose.Schema.Types.ObjectId],
+    ref: "User",
+  }],
+},
+{
+  timestamps: true
+}
+)
+
+export const Workspace = mongoose.model("Workspace", workspaceSchema);
