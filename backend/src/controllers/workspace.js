0fb2277c6f1890e6c774742e57a43ac4e7216363
FOA-developer <flourishagbanoma@gmail.com>
2026-06-18 16:50:23 +0100

    feat: refactor createWorkspace function to remove unused member verification and streamline workspace creation

--- backend/src/controllers/workspace.js
+++ backend/src/controllers/workspace.js
@@ -2,43 +2,34 @@ import { Workspace } from "../models/workspace.js";
 
 const createWorkspace = async (req, res) => {
   try{
-    const{name, members} = req.body
+    const{name} = req.body
     if(!name){
       return res.status(400).json({
         message: "You need to put the required information"
       })
     }
-
-    const verifyMembers = await (members) => {
-      for(let i = 0; i >= members.length; i++){
-        const existing =await members[i].findOne({_id: members[i]});
-        if(!exiting){
-          return res.staus(400).json({
-            message: "You can only uuse registered users"
-          })
-        }
-      }
-    }
-
-    if (!verifyMembers){
-        return res.status(400).json({
-          message : "use registered users"
+    const owner = req.user;
+      const workspace = await Workspace.create({
+        name,
+         owner,
+        members : [owner]
+      })
+        return res.status(200).json({
+          message: "Workspace created successfully",
+          workspace: {
+          id: workspace._id
+          },
+          members: workspace.members,
+          owner: workspace.owner
         })
-      }else{
-        const workspace = await workspace.create({
-          name,
-          owner,
-          workspace._id,
-          members
-        })
-        return res.satus(200).json({
-          mesage: "Workspace create succesfully"
-        })
-      }
 
   } catch(err){
     return res.status(500).json({
       message : `Internal server error ${err}`
     })
   }
+}
+
+export {
+  createWorkspace,
 }
