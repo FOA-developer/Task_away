4aa46eb08f6f1954061f1dcf65e4143198d8907f
FOA-developer <flourishagbanoma@gmail.com>
2026-06-18 16:46:51 +0100

    feat: add workspace router for handling workspace-related API requests

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors"
 import authRouter from "./src/routes/user.js"
+import Workspacerouter from "./src/routes/workspace.js"
 
 const app = express();
 
 app.use(cors())
 app.use(express.json())
 app.use("/api/auth", authRouter )
+app.use("/api/workspace", Workspacerouter)
+
 
 
 
