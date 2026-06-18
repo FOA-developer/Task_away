9d310e182197b4fd5bb319a9029adff945fe082a
FOA-developer <flourishagbanoma@gmail.com>
2026-06-18 16:48:58 +0100

    feat: add task router for handling task-related API requests

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors"
 import authRouter from "./src/routes/user.js"
 import Workspacerouter from "./src/routes/workspace.js"
+import taskRouter from "./src/routes/task.js"
 
 const app = express();
 
@@ -9,8 +10,7 @@ app.use(cors())
 app.use(express.json())
 app.use("/api/auth", authRouter )
 app.use("/api/workspace", Workspacerouter)
-
-
+app.use("/api/task", taskRouter)
 
 
 export default app;
