d38929587fdf7946461a416d7190c73059e8ab35
FOA-developer <flourishagbanoma@gmail.com>
2026-08-10 17:24:21 +0100

    feat: implement update and delete task functionality with ownership validation

--- backend/src/controllers/task.js
+++ backend/src/controllers/task.js
@@ -70,7 +70,87 @@ const getTask = async (req, res) => {
   
 }
 
+const updateTask = async (req, res) => {
+  try{
+    const { description, status, title, assignedTo } = req.body
+    const task = await Task.findById(req.params.taskId)
+    if(!task){
+      return res.status(403).json({
+        message : "Not a valid task"
+      })
+    }
+
+    const owner = task.owner
+    if(req.user._id.toString() !== owner.toString()){
+      return res.status(403).json({
+        message: "Only owners can update task"
+      })
+    }
+
+
+    const fields = { title, description, status, assignedTo }
+    const updates = {}
+
+    for (const key in fields) {
+      if (fields[key] !== undefined) {
+        updates[key] = fields[key]
+        if (key == "assignedTo"){
+          const workspace = await Workspace.findById(task.workspace)
+          const verify = await workspace.members.includes(assignedTo)
+          if(!verify){
+            return res.status(403).json({
+              message : "They must be mebers of the workspace"
+            })
+          }
+        }
+      }
+    }
+
+    const updated = await Task.findByIdAndUpdate(task._id, updates, {new: true})
+    return res.status(200).json({
+      message: "Task updated successfully",
+      task: updated
+    })
+
+  }catch(err){
+    return res.status(500).json({
+      message: `Internal server error ${err}`
+    })
+  }
+  
+}
+
+const deleteTask = async (req, res) => {
+  try{
+    const task = await Task.findById(req.params.taskId)
+    if(!task){
+      return res.status(400).json({
+        message: "Not a valid task"
+      })
+    }
+    const owner = task.owner
+    if(owner.toString() !== req.user._id.toString()){
+      return res.status(403).json({
+        message: "Only the owner fo a task can delete the task"
+      })
+    }
+
+    const deletedTask = await Task.findByIdAndDelete(task._id)
+    return res.status(200).json({
+      message: "Task deleted successfully"
+    })
+
+
+  }catch(err){
+    return res.status(500).json({
+      message: `Internal server error ${err}`
+    })
+  }
+}
+
 export {
   createTask,
-  getTask
+  getTask,
+  updateTask,
+  deleteTask
 }
