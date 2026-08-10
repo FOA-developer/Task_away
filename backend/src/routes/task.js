5f6974df31be313551cc3ede32b3d736e9575199
FOA-developer <flourishagbanoma@gmail.com>
2026-08-10 23:41:39 +0100

    feat: added task creation and retrieval routes with token verification

--- backend/src/routes/task.js
+++ backend/src/routes/task.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
+import verifyToken from "../middleware/auth.js";
+import { createTask, getTask } from "../controllers/task.js";
 
 const taskRouter = Router();
+
+taskRouter.post("/create_task", verifyToken, createTask);
+taskRouter.get("/get_task", verifyToken, getTask);
 
 export default taskRouter;
