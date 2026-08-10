905826056b0438d672fee9ab66cbecca651b8e30
FOA-developer <flourishagbanoma@gmail.com>
2026-08-10 23:56:51 +0100

    feat: add workspace retrieval and switching routes with token verification

--- backend/src/routes/workspace.js
+++ backend/src/routes/workspace.js
@@ -1,9 +1,12 @@
 import {Router} from 'express';
-import { createWorkspace } from '../controllers/workspace.js';
+import { createWorkspace, switchWorkspace, getWorkspace } from '../controllers/workspace.js';
 import verifyToken from "../middleware/auth.js"
 
 const Workspacerouter = Router();
 
 Workspacerouter.post("/create_workspace", verifyToken , createWorkspace);
+Workspacerouter.get("/get_workspace", verifyToken, getWorkspace);
+Workspacerouter.patch("/switch_workspace", verifyToken, switchWorkspace);
+
 
 export default Workspacerouter;
