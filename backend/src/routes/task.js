e4f010ccdc7803ef7409078334d174ee4d6e5a1c
FOA-developer <flourishagbanoma@gmail.com>
2026-08-11 12:30:37 +0100

    feat: add update and delete task routes with token verification

--- backend/src/routes/task.js
+++ backend/src/routes/task.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import verifyToken from "../middleware/auth.js";
-import { createTask, getTask } from "../controllers/task.js";
-
+import { createTask, getTask, updateTask, deleteTask} from "../controllers/task.js";
 const taskRouter = Router();
 
 taskRouter.post("/create_task", verifyToken, createTask);
 taskRouter.get("/get_task", verifyToken, getTask);
+taskRouter.patch("/update_task/:taskId", verifyToken, updateTask);
+taskRouter.delete("/delete_task/:taskId", verifyToken, deleteTask);
 
 export default taskRouter;
