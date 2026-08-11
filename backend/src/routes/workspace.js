80a1bbc8baf65423fe1d5d7daa095b9652c0224d
FOA-developer <flourishagbanoma@gmail.com>
2026-08-11 12:32:33 +0100

    feat: added the routes for the add_member controller

--- backend/src/routes/workspace.js
+++ backend/src/routes/workspace.js
@@ -1,12 +1,12 @@
 import {Router} from 'express';
-import { createWorkspace, switchWorkspace, getWorkspace } from '../controllers/workspace.js';
+import { createWorkspace, switchWorkspace, getWorkspace, addMember} from '../controllers/workspace.js';
 import verifyToken from "../middleware/auth.js"
-
 const Workspacerouter = Router();
 
 Workspacerouter.post("/create_workspace", verifyToken , createWorkspace);
 Workspacerouter.get("/get_workspace", verifyToken, getWorkspace);
 Workspacerouter.patch("/switch_workspace", verifyToken, switchWorkspace);
+Workspacerouter.patch("/add_member", verifyToken, addMember);
 
 
 export default Workspacerouter;
