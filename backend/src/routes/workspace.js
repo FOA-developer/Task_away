ea5c1c0556058488bb0c6158737085cef7e4b368
FOA-developer <flourishagbanoma@gmail.com>
2026-08-17 15:39:22 +0100

    feat: add getCurrentWorkspace route to retrieve the current workspace

--- backend/src/routes/workspace.js
+++ backend/src/routes/workspace.js
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { createWorkspace, switchWorkspace, getWorkspace, addMember} from '../controllers/workspace.js';
+import { createWorkspace, switchWorkspace, getWorkspace, addMember, getCurrentWorkspace} from '../controllers/workspace.js';
 import verifyToken from "../middleware/auth.js"
 const Workspacerouter = Router();
 
@@ -7,6 +7,6 @@ Workspacerouter.post("/create_workspace", verifyToken , createWorkspace);
 Workspacerouter.get("/get_workspace", verifyToken, getWorkspace);
 Workspacerouter.patch("/switch_workspace", verifyToken, switchWorkspace);
 Workspacerouter.patch("/add_member", verifyToken, addMember);
-
+Workspacerouter.get("/get_currentWorkspace", verifyToken, getCurrentWorkspace)
 
 export default Workspacerouter;
