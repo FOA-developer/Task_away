9da4bb43c80fc6d79a03334647720eb084a73c2a
FOA-developer <flourishagbanoma@gmail.com>
2026-08-21 13:08:01 +0100

    feat: add DayPanel component for displaying content in a modal-like overlay

---
+++ frontend/src/components/shared/DayPanel.jsx
@@ -0,0 +1,23 @@
+import { X } from "lucide-react";
+
+const DayPanel = ({children, onClose}) => {
+  return (
+    <div 
+      className="fixed inset-0 bg-black/50 backdrop-blur-sm z-40 flex md:justify-end items-center justify-center"
+      onClick={onClose}
+    >
+      <div 
+        onClick={(e) => e.stopPropagation()}
+        className="bg-white flex flex-col w-11/12 max-w-md rounded-2xl md:w-96 md:h-screen md:rounded-none md:max-w-none">
+        <div className="flex flex-col py-10 px-10">
+          <div>
+            <button onClick={onClose} className="self-end text-primary"><X size={26} /></button>
+          </div>
+          <div>{children}</div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DayPanel;
