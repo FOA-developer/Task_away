93d5c59e33c4528c0aabba9870c75774ea82076b
FOA-developer <flourishagbanoma@gmail.com>
2026-08-21 15:44:40 +0100

    feat: add Block component for reusable UI element with logout functionality

---
+++ frontend/src/components/shared/Block.jsx
@@ -0,0 +1,12 @@
+const Block = ({children, onLogout}) => {
+  return (
+    <div className="shadow-sm hover:shadow-md rounded-lg text-xs md:text-sm text-primary bg-whiter active:shadow-lg transition-shadow duration-150 cursor-pointer w-full p-3 flex flex-col" onClick={(e) => { 
+      e.stopPropagation;
+      onLogout()
+    }}>
+      {children}
+    </div>
+  )
+}
+
+export default Block;
