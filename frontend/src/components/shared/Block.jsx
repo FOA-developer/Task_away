ba79741a9f224c64da505b657d9f3d7470a47275
FOA-developer <flourishagbanoma@gmail.com>
2026-08-21 16:16:13 +0100

    fix: correct stopPropagation method call in Block component

--- frontend/src/components/shared/Block.jsx
+++ frontend/src/components/shared/Block.jsx
@@ -1,7 +1,7 @@
 const Block = ({children, onLogout}) => {
   return (
     <div className="shadow-sm hover:shadow-md rounded-lg text-xs md:text-sm text-primary bg-whiter active:shadow-lg transition-shadow duration-150 cursor-pointer w-full p-3 flex flex-col" onClick={(e) => { 
-      e.stopPropagation;
+      e.stopPropagation();
       onLogout()
     }}>
       {children}
