2deae8cf6a54c5c6fd0c5af5d9609876e424672d
FOA-developer <flourishagbanoma@gmail.com>
2026-08-22 17:58:00 +0100

    fix: update delete_member route to include userId parameter

--- backend/src/routes/workspace.js
+++ backend/src/routes/workspace.js
@@ -8,6 +8,6 @@ Workspacerouter.get("/get_workspace", verifyToken, getWorkspace);
 Workspacerouter.patch("/switch_workspace", verifyToken, switchWorkspace);
 Workspacerouter.patch("/add_member", verifyToken, addMember);
 Workspacerouter.get("/get_currentWorkspace", verifyToken, getCurrentWorkspace);
-Workspacerouter.delete("/delete_member", verifyToken, removeMember);
+Workspacerouter.delete("/delete_member/:userId", verifyToken, removeMember);
 
 export default Workspacerouter;
