74bf8a33b61a390f036e85848e93853fbc6b1e1d
FOA-developer <flourishagbanoma@gmail.com>
2026-08-23 19:23:04 +0100

    fix: add token verification to get_user endpoint

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { loginUser, registerUser, getMe} from "../controllers/user.js"
+import { loginUser, registerUser, getMe} from "../controllers/user.js";
+import verifyToken from "../middleware/auth.js"
 
 const authRouter = Router();
 authRouter.post("/login", loginUser)
 authRouter.post("/register", registerUser)
-authRouter.get("/get_user", getMe)
+authRouter.get("/get_user", verifyToken, getMe)
 
 export default authRouter;
