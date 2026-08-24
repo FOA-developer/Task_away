14e3d70ba885dfcd9093213a43de787eb111e63c
FOA-developer <flourishagbanoma@gmail.com>
2026-08-24 11:12:18 +0100

    feat: add Features and Footer components to LandingPage for enhanced content display

--- frontend/src/pages/public/LandingPage.jsx
+++ frontend/src/pages/public/LandingPage.jsx
@@ -1,11 +1,15 @@
 import Hero from "../../components/landing/Hero";
 import HowItWorks from "../../components/landing/HowItWorks";
+import Features from "../../components/landing/Features";
+import Footer from "../../components/landing/Footer";
 
 const LandingPage = ( ) => {
   return(
     <>
     <Hero/>
     <HowItWorks/>
+    <Features/>
+    <Footer/>
     </>
   )
 }
